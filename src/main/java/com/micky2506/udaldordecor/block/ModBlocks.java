2fbb132e92c03002bb4fd5cdf8a42c7d3c3971de
Mark Simulacrum <minecraft.micky2506@gmail.com>
2014-07-09 08:29:49 -0600

    Add recipe for tracking block.

--- src/main/java/com/micky2506/udaldordecor/block/ModBlocks.java
+++ src/main/java/com/micky2506/udaldordecor/block/ModBlocks.java
@@ -3,6 +3,7 @@ package com.micky2506.udaldordecor.block;
 import com.micky2506.udaldordecor.lib.Names;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
 public class ModBlocks
@@ -16,5 +17,6 @@ public class ModBlocks
         GameRegistry.registerBlock(displayCase, ItemBlockDisplayCase.class, Names.displayCase);
 
         GameRegistry.addShapedRecipe(new ItemStack(displayCase, 1, 0), "ggg", "gig", "ggg", 'i', Blocks.glass_pane, 'g', Blocks.glass);
+        GameRegistry.addShapedRecipe(new ItemStack(displayCase, 1, 1), "gsg", "gig", "gsg", 'i', Blocks.glass_pane, 'g', Blocks.glass, 's', Items.string);
     }
 }
